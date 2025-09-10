2cd3ee51830d6e9fa8b9e459bd55803616c32e88
Felix Olmo <felixolmo@linkit.digital>
2025-09-10 09:12:33 -0400

    remove CSP for embedding

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  async headers() {
-    return [
-      {
-        source: "/:path*",
-        headers: [
-          {
-            key: "Content-Security-Policy",
-            value: "frame-ancestors 'self' https://linkit.digital https://www.linkit.digital https://*.linkit.digital"
-          }
-        ]
-      }
-    ];
-  }
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
